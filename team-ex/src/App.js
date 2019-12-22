b8ebb75ddb62058d22ebcbd82a227f2927264cba
Aviv2991 <55682481+Aviv2991@users.noreply.github.com>
2019-12-22 16:17:07 +0200

    phase2 add

--- team-ex/src/App.js
+++ team-ex/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-
+import Phase2 from './components/newComponents/phase2.jsx';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+  }from 'react-router-dom';
 import './App.css';
 
 export default class App extends Component {
@@ -21,7 +27,13 @@ export default class App extends Component {
     render () {
         return ( 
         <div className = "App">
-            
+            <Router>
+                <Switch>
+                    <Route path='/'>
+                        <Phase2/>
+                    </Route>
+                </Switch>
+            </Router>
         </div>
         )
     }
