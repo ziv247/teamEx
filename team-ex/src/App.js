3ae7c1e680253fd977e4334ab31bbef775910523
Ziv <ziv2471993@gmail.com>
2019-12-23 09:10:25 +0200

    Morning upload

--- team-ex/src/App.js
+++ team-ex/src/App.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import Phase2 from './components/newComponents/phase2.jsx';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-  }from 'react-router-dom';
-=======
 
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> master
 import './App.css';
 import Wizard from './components/wizard';
 import {
@@ -20,7 +9,9 @@ import {
     Route,
   } from "react-router-dom";
 
-import Phase3 from './components/phase3/phase3';
+import Phase1 from './components/phase1';
+import Phase2 from './components/phase2';
+import Phase3 from './components/phase3';
 
 
 export default class App extends Component {
@@ -38,26 +29,17 @@ export default class App extends Component {
 
         <div className = "App">
             <Router>
-<<<<<<< HEAD
-                <Switch>
-                    <Route path='/'>
-                        <Phase2/>
-                    </Route>
-                </Switch>
-            </Router>
-=======
       
 
       <Switch>
         <Route path="/" exact children={<Wizard/>} />
-        <Route path="/phase1" children={<h3>phase1</h3>} />
-        <Route path="/phase2" children={<h3>phase2</h3>} />
-        <Route path="/phase3" children={<h3>phase3</h3>} />
+        <Route path="/phase1" children={<Phase1/>} />
+        <Route path="/phase2" children={<Phase2/>} />
+        <Route path="/phase3" children={<Phase3/>} />
         <Route path="/summary" children={<h3>summary</h3>} />
       </Switch>
     </Router>
             
->>>>>>> master
         </div>
         )
 
